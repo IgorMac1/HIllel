64d267970e2a7dbc8327aa01d78cffd689cda7a9
igormakeev <makieiv@yahoo.com>
2022-08-27 14:23:49 +0300

    homework

--- index.php
+++ index.php
@@ -2,22 +2,5 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 
-use Classes\Currency;
-use Classes\Money;
-
-
-try {
-    $money = new Money(35, new Currency('uah'));
-    $money1 = new Money(30, new Currency('usd'));
-    $money2 = new Money(25, new Currency('UAH'));
-    dd($money->add($money2));
-
-
-} catch (InvalidArgumentException $exception) {
-    echo $exception->getMessage();
-}
-
-
-
 
 
